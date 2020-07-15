e03f3873212a62c8039158a3c7bc17959ad5de10
Carlos Cuautle <cuautlecg@hotmail.com>
2020-07-15 16:43:26 -0500

    Creación de pantallas de Login y Registro

---
+++ app/src/main/java/com/cuautledev/uberclone/SelectOptionAuthActivity.java
@@ -0,0 +1,14 @@
+package com.cuautledev.uberclone;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SelectOptionAuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_option_auth);
+    }
+}
